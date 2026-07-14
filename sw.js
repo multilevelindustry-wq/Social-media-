f3e9c89c82ed2233de96efdbb73322132a2e2656
multilevelindustry-wq <multilevelindustry@gmail.com>
2026-07-14 18:32:17 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "creatorhub-v1";
+
+const FILES = [
+    "./",
+    "./index.html"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+});
