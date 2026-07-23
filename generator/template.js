c41b29a266eda0c93aca62f2bcaa884d7d442264
multilevelindustry-wq <multilevelindustry@gmail.com>
2026-07-23 09:05:06 +0100

    Implement buildPostHTML function for post redirection
    Add a function to build HTML for a blog post with redirection.

--- generator/template.js
+++ generator/template.js
@@ -1,1 +1,37 @@
+export function buildPostHTML(post){
 
+return `<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>${post.title}</title>
+
+<meta name="description"
+
+content="${post.description}">
+
+<script>
+
+location.replace(
+
+"post.html?id=${post.postId}"
+
+);
+
+</script>
+
+</head>
+
+<body>
+
+Redirecting...
+
+</body>
+
+</html>`;
+
+}
